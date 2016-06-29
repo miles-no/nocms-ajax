6b909b13297f24c7e02a7e3e165433e885a92aea
Jørgen Johansen <jorgen@jorgenjohansen.com>
2016-06-29 09:45:49 +0200

    Removed sendMessage from ajax api

--- src/index.js
+++ src/index.js
@@ -1,24 +1,7 @@
 
 const ajax = require('./ajax');
 
-const sendMessage = (messageType, messageData, o, callback) => {
-  let options = o;
-  let cb = callback;
-  const messageObj = {
-    messageType,
-    data: messageData,
-  };
-
-  if (typeof options === 'function') {
-    cb = options;
-    options = {};
-  }
-
-  ajax.post('/api/message', messageObj, options, cb);
-};
-
 module.exports = {
   get: ajax.get,
   post: ajax.post,
-  sendMessage
 };
